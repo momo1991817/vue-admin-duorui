5e5c61b683ed138fb3d108015582ddeafcb7dff5
maodunWorld <944126367>
2019-07-19 18:09:41 +0800

    增加图表

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">
+      name: {{ name }}
+      <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+// 引入基本模板
+const echarts = require('echarts/lib/echarts')
+// 引入柱状图组件
+require('echarts/lib/chart/bar')
+// 引入提示框和title组件
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
 
 export default {
   name: 'Dashboard',
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(document.getElementById('myChart'))
+      // 绘制图表
+      myChart.setOption({
+        title: { text: 'ECharts 入门示例' },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }
+  },
+  mounted() {
+    this.drawLine()
+  },
   computed: {
     ...mapGetters([
       'name'
