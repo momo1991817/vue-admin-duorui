0cc63ed00533d5c8ad7a74c84325bce12654ccb7
wangying <978983149@qq.com>
2019-07-22 11:36:01 +0800

    no message

--- src/views/UserManage/index.vue
+++ src/views/UserManage/index.vue
@@ -1,30 +1,38 @@
 <template>
   <div class="app-container">
     <div class="dashboard-text">
-      <el-table
-      :data="tableData"
-      border
-      style="width: 100%">
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-        <el-table-column label="Token" prop="date">
+      <el-table :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column prop="date"
+          label="openId"
+          width="180">
         </el-table-column>
-        <el-table-column label="用户头像" prop="date">
+        <el-table-column label="Token"
+          prop="date">
+        </el-table-column>
+        <el-table-column label="用户头像"
+          prop="date">
         </el-table-column>
         <el-table-column label="性别">
           <template slot-scope="scope">
           </template>
         </el-table-column>
-        <el-table-column label="用户昵称" prop="date">
+        <el-table-column label="用户昵称"
+          prop="date">
         </el-table-column>
-        <el-table-column label="创建时间" prop="date">
+        <el-table-column label="创建时间"
+          prop="date">
         </el-table-column>
-        <el-table-column label="更新时间" prop="date">
+        <el-table-column label="更新时间"
+          prop="date">
         </el-table-column>
       </el-table>
+    </div>
+    <div class="block">
+      <el-pagination layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
     </div>
   </div>
 </template>
